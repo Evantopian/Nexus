252474be59731e69c51bc20bdf06f62b99276f9b
Evan Huang <evanhuang47@gmail.com>
2025-03-02 18:19:23 -0500

    add: signup page and assets

--- frontend/nexus-web/src/routes/index.tsx
+++ frontend/nexus-web/src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { RouteObject } from "react-router-dom";
 import Home from "@/components/home";
+import SignUp from "@/components/auth/SignUp";
+import Login from "@/components/auth/Login";
 import MainLayout from "@/layouts/MainLayout";
+import AuthLayout from "@/layouts/AuthLayout";
 
 
 const routes: RouteObject[] = [
@@ -8,6 +11,14 @@ const routes: RouteObject[] = [
     path: "/",
     element: <MainLayout />,
     children: [{ index: true, element: <Home /> }],
+  },
+  {
+    path: "/",
+    element: <AuthLayout />,
+    children: [
+      { path: "login", element: <Login /> },
+      { path: "signup", element: <SignUp /> },
+    ],
   },
 ];
 
