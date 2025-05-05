5d34fb655accb8fd33f4d8d3d78fd5037b1d5fe3
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2025-05-05 13:17:05 -0400

    Added chat for hosting changes

---
+++ backend/chat_system/lib/chat_system_web/channels/room_channel.ex
@@ -0,0 +1,22 @@
+defmodule ChatSystemWeb.RoomChannel do
+  use Phoenix.Channel
+
+  def join("room:" <> _rest, _params, socket) do
+    {:ok, socket}
+  end
+
+  def handle_in("message:new", %{"user" => user, "body" => body}, socket) do
+    broadcast!(socket, "message:new", %{user: user, body: body})
+    {:noreply, socket}
+  end
+
+  def handle_in("typing:start", %{"user" => user}, socket) do
+    broadcast_from!(socket, "typing:start", %{user: user})
+    {:noreply, socket}
+  end
+
+  def handle_in("typing:stop", %{"user" => user}, socket) do
+    broadcast_from!(socket, "typing:stop", %{user: user})
+    {:noreply, socket}
+  end
+end
