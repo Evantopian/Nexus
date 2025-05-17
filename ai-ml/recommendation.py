b8e66dd3a532c030968902fcd160ca1dffd84a4a
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2025-05-17 18:16:14 -0400

    Added cors settings

--- ai-ml/recommendation.py
+++ ai-ml/recommendation.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 import numpy as np
 import ast
@@ -14,6 +15,14 @@ from sqlalchemy.exc import OperationalError
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 load_dotenv()
 '''
